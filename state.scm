4332e5755382e3c1a1dfd5f9517af126c60e7427
David Pendergast <dlp75@case.edu>
2016-02-10 16:37:56 -0500

    added state.scm function headers

---
+++ state.scm
@@ -0,0 +1,21 @@
+; state.scm
+
+(define remove
+  (lambda (var s)))
+
+(define declare
+  (lambda (var s)
+
+(define assign
+  (lambda (var value s)
+
+(define get
+  (lambda (var s)
+
+(define assigned?
+  (lambda (var s)
+
+(define declared?
+  (lambda (var s)
+    
+    
