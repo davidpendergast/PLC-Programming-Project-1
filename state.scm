11b183c2b7e15a2355ee7766a526184168fe5051
Joel Kalos <jdk138@case.edu>
2016-02-10 16:53:10 -0500

    added remove and appropriate abstractions

--- state.scm
+++ state.scm
@@ -1,8 +1,5 @@
 ; state.scm
 
-(define remove
-  (lambda (var s)))
-
 (define declare
   (lambda (var s)
 
@@ -18,4 +15,21 @@
 (define declared?
   (lambda (var s)
     
-    
+(define state-cdr
+  (lambda (s)
+    (list (cdar s) (cdadr s))))
+
+(define state-car
+  (lambda (s)
+    (list (caar s) (caadr s))))
+
+(define state-cons
+  (lambda (v s)
+    (list (cons (car v) (car s)) (cons (cadr v) (cadr s)))))
+
+(define state-remove
+  (lambda (var s)
+    (cond
+      ((null? (car s)) (error var "variable not declared"))
+      ((eq? (caar s) var) (state-cdr s))
+      (else (state-cons (state-car s) (state-remove var (state-cdr s)))))))
