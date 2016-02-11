742e5f89eacf7bb415c5cf48d749f958e858f954
David Pendergast <dlp75@case.edu>
2016-02-10 19:04:50 -0500

    finished m_assign

--- mstate.scm
+++ mstate.scm
@@ -14,64 +14,6 @@
 ;      
 ;      )))
  
-; takes a statement and a state, and returns the new state following the statement's execution.
-(define M_state
-  (lambda (stmt s)
-    (cond
-      ((declare? stmt) (M_declare (cadr stmt) s))
-      ((declare_with_assign? stmt) (M_declare_with_assign (cadr stmt) (caddr stmt) s))
-      (else '())
-      )))
-
-(define M_declare
-  (lambda (variable s)
-    (state-declare variable s)))
-
-(define M_declare_with_assign
-  (lambda (variable expression s)
-    (state-assign variable (M_value expression (state-declare variable s)) (state-declare variable s))))
-
-; takes statement of form (= variable expression), returns new state
-(define M_assign
-  (lambda (variable expression s)
-    (if (state-declared? variable s)
-        (state-assign))))
-
-; takes statement of form ('return expression), returns new state
-(define M_return
-  (lambda (expression s)
-    #f))
-
-(define M_if_else
-  (lambda (condition then-stmt else-stmt s)
-    #f))
-
-(define M_if
-  (lambda (condition then-stmt s)
-    #f))
-
-(define M_while
-  (lambda (condition loop-body s)
-    #f))
-
-;-------------------------------------------;
-; Functions to determine types of statement |
-;-------------------------------------------;
-(define declare?
-  (lambda (stmt)
-    (if (eq? (length stmt) 2)
-        (eq? 'var (car stmt))
-        #f)))
-
-(define declare_with_assign?
-  (lambda (stmt)
-    (if (eq? (length stmt) 3)
-        (eq? 'var (car stmt))
-        #f)))
-
-(M_state '(var x) '(()()))
-; (M_state '(var x 10) '((y z)(15 40))) ; M_value not ready
-
 (define operator car)
 (define operand1 cadr)
 (define operand2 caddr)
@@ -103,5 +45,73 @@
       ((eq? (operator expression) '||) (or (M_boolean (operand1 expression) s) (M_boolean (operand2 expression) s)))
       ((eq? (operator expression) '!) (not (M_boolean (operand1 expression) s)))
       (else (error 'unknown "unknown expression")))))
+
+(define M_state
+  (lambda (stmt s)
+    (cond
+      ((declare? stmt) (M_declare (cadr stmt) s))
+      ((declare_with_assign? stmt) (M_declare_with_assign (cadr stmt) (caddr stmt) s))
+      ((assign? stmt) (M_assign (cadr stmt) (caddr stmt) s))
+      (else '())
+      )))
+
+(define M_declare
+  (lambda (variable s)
+    (state-declare variable s)))
+
+(define M_declare_with_assign
+  (lambda (variable expression s)
+    (state-assign variable (M_value expression (state-declare variable s)) (state-declare variable s))))
+
+; takes statement of form (= variable expression), returns new state
+(define M_assign
+  (lambda (variable expression s)
+    (if (state-declared? variable s)
+        (state-assign variable (M_value expression s) s)
+        (error variable "variable is not declared."))))
+
+(define M_return
+  (lambda (expression s)
+    #f))
+
+(define M_if_else
+  (lambda (condition then-stmt else-stmt s)
+    #f))
+
+(define M_if
+  (lambda (condition then-stmt s)
+    #f))
+
+(define M_while
+  (lambda (condition loop-body s)
+    #f))
+
+;-------------------------------------------;
+; Functions to determine types of statement |
+;-------------------------------------------;
+(define declare?
+  (lambda (stmt)
+    (if (eq? (length stmt) 2)
+        (eq? 'var (car stmt))
+        #f)))
+
+(define declare_with_assign?
+  (lambda (stmt)
+    (if (eq? (length stmt) 3)
+        (eq? 'var (car stmt))
+        #f)))
+
+(define assign?
+  (lambda (stmt)
+    (if (eq? (length stmt) 3)
+        (eq? '= (car stmt))
+        #f)))
+
+(M_state '(var x) '(()()))
+(M_state '(var x 10) '((y z)(15 40)))
+(M_state '(= x 20) '((x) (10)))
+(M_state '(= x 20) '((y x z) (0 () 6)))
+
+
   
-    
+;(M_boolean '(> 9 0) '(()()))
