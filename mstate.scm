f37e361511968a06df66ba551d5ca48f2162624e
David Pendergast <dlp75@case.edu>
2016-02-11 12:32:08 -0500

    m_state completed

--- mstate.scm
+++ mstate.scm
@@ -1,18 +1,5 @@
 (load "simpleParser.scm")
 (load "state.scm")
-; mstate.scm
-
-;(define interpret
-;  (lambda (filename)
-;    (runcode (parse filename) '('()'()) )))
-    
-;(define runcode
-;  (lambda (tree s)
-;    (cond
-;      ((null? tree) s) ; shouldn't happen
-;      (else "not finished")
-;      
-;      )))
  
 (define operator car)
 (define operand1 cadr)
@@ -28,7 +15,7 @@
       ((eq? (operator expression) '*) (* (M_value (operand1 expression) s) (M_value (operand2 expression) s)))
       ((eq? (operator expression) '/) (quotient (M_value (operand1 expression) s) (M_value (operand2 expression) s)))
       ((eq? (operator expression) '%) (remainder (M_value (operand1 expression) s) (M_value (operand2 expression) s)))
-      (else (error 'unknown "unknown expression")))))
+      (else (error 'unknown expression "unknown expression")))))
 
 (define M_boolean
   (lambda (expression s)
@@ -44,7 +31,19 @@
       ((eq? (operator expression) '&&) (and (M_boolean (operand1 expression) s) (M_boolean (operand2 expression) s)))
       ((eq? (operator expression) '||) (or (M_boolean (operand1 expression) s) (M_boolean (operand2 expression) s)))
       ((eq? (operator expression) '!) (not (M_boolean (operand1 expression) s)))
-      (else (error 'unknown "unknown expression")))))
+      (else (error 'unknown expression "unknown expression")))))
+
+(define execfile
+  (lambda (filename)
+    (interpret (parser filename) '(()()))))
+
+; takes a parse tree
+(define interpret
+  (lambda (parsetree s)
+    (cond
+      ((number? s) s) ; return value reached
+      ((null? parsetree) (error "parse tree reached no return statement."))
+      (else (interpret (cdr parsetree) (M_state (car parsetree) s))))))
 
 (define M_state
   (lambda (stmt s)
@@ -52,9 +51,10 @@
       ((declare? stmt) (M_declare (cadr stmt) s))
       ((declare_with_assign? stmt) (M_declare_with_assign (cadr stmt) (caddr stmt) s))
       ((assign? stmt) (M_assign (cadr stmt) (caddr stmt) s))
-      ((if? stmt) (M_if (cadr stmt) (caddr stmt)))
-      ((if_with_else? stmt) (M_if_else (cadr stmt) (caddr stmt) (cadddr stmt)))
+      ((if? stmt) (M_if (cadr stmt) (caddr stmt) s))
+      ((if_with_else? stmt) (M_if_else (cadr stmt) (caddr stmt) (cadddr stmt) s))
       ((while? stmt) (M_while (cadr stmt) (caddr stmt) s))
+      ((return? stmt) (M_return (cadr stmt) s))
       (else '())
       )))
 
@@ -68,21 +68,24 @@
 
 (define M_assign
   (lambda (variable expression s)
-    (if (state-declared? variable s)
-        (state-assign variable (M_value expression s) s)
-        (error variable "variable is not declared."))))
+    (state-assign variable (M_value expression s) s)))
 
+; returns a value (number), not a state
 (define M_return
   (lambda (expression s)
-    #f))
+    (M_value expression s)))
 
 (define M_if_else
   (lambda (condition then-stmt else-stmt s)
-    #f))
+   (if (M_boolean condition s)
+        (M_state then-stmt s)
+        (M_state else-stmt s))))
 
 (define M_if
   (lambda (condition then-stmt s)
-    #f))
+    (if (M_boolean condition s)
+        (M_state then-stmt s)
+        s)))
 
 (define M_while
   (lambda (condition loop-body s)
@@ -135,11 +138,17 @@
         (eq? 'while (car stmt))
         #f)))
 
+; tests
 (M_state '(var x) '(()()))
 (M_state '(var x 10) '((y z)(15 40)))
 (M_state '(= x 20) '((x) (10)))
 (M_state '(= x 20) '((y x z) (0 () 6)))
 (M_state '(while (< i 10) (= i (+ i x))) '((i x)(0 3)))
+(M_state '(if (< x 2) (= x 2)) '((x)(1)))
+(M_state '(if (>= x 2) (= x 7) (= x (+ x 1))) '((x)(0)))
+
+(parser "testfile.txt")
+(execfile "testfile.txt")
 
 
   
